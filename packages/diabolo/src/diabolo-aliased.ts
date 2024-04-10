3fa90e8ae35a1909b35a61001fb02a143b85cda3
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-04-10 18:36:59 +0200

    Improve JSR score

--- packages/diabolo/src/diabolo-aliased.ts
+++ packages/diabolo/src/diabolo-aliased.ts
@@ -1,3 +1,18 @@
+/**
+ * @module diabolo
+ *
+ * This module provides the same simple dependency injection utilities as the default export, but with shorter names.
+ *
+ * | Original | Alias |
+ * | --- | --- |
+ * | `Service` | `Srv` |
+ * | `createService` | `srv` |
+ * | `lazyCreateServiceImpl` | `impl` |
+ * | `createFunction` | `fn` |
+ * | `requireService` | `req` |
+ * | `provide` | `prov` |
+ */
+
 export { createFunction as fn } from './diabolo/create-function'
 export { createService as srv } from './diabolo/create-service'
 export { lazyCreateServiceImpl as impl } from './diabolo/lazy-create-service-impl'
